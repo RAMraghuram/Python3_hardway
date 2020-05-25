1c73f514026ab866dc858b8a8b78037564341d70
RAMraghuram <ram309591@gmail.com>
2020-05-25 20:25:16 +0530

    adding the folders to the git

---
+++ Python3-the-hard-way/argvint.py
@@ -0,0 +1,8 @@
+from sys import argv
+script, int1, int2, int3, int4 = argv
+
+print('The script name is:', script)
+print('The sum of first two integers is:', int1+int2)
+print('The sum of next two integers is:', int2+int3)
+print('The sum of next two integers is:', int3+int4)
+
